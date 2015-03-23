54627c9fda21387f1a6ffbfc4e2b3e08dd524f7b
maha54 <54.maka@gmail.com>
2015-03-23 15:59:12 +0100

    add reset submit button

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -1,14 +1,14 @@
 
-
-	<div class="clearfix">
-		{{ Form::open(array('route' => array( $route ),'method' => 'GET', 'class' => 'form-horizontal col-lg-3' )) }}
-		<div class="form-group">
-			<div class="input-group">
-				{{ Form::text('search', $search, ['class' => 'form-control', 'placeholder' => Lang::get('datatable::datatable.form.placeholder')]) }}
-				<div class="input-group-btn">
-					{{ Form::submit( Lang::get('datatable::datatable.form.submit') , ['class' => 'btn btn-primary']) }}
-				</div>
+<div class="clearfix">
+	{{ Form::open(array('route' => array( $route ),'method' => 'GET', 'class' => 'form-horizontal col-lg-3' )) }}
+	<div class="form-group">
+		<div class="input-group">
+			{{ Form::text('search', $search, ['class' => 'form-control', 'placeholder' => Lang::get('datatable::datatable.form.placeholder')]) }}
+			<div class="input-group-btn">
+				{{ Form::submit( Lang::get('datatable::datatable.form.submit') , ['class' => 'btn btn-default']) }}
+				{{ Form::submit( Lang::get('datatable::datatable.form.reset') , ['class' => 'btn btn-link', 'name' => 'reset']) }}
 			</div>
 		</div>
-		{{ Form::close() }}
 	</div>
+	{{ Form::close() }}
+</div>
